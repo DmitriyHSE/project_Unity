d28d24c3b83079a2fcf4199f0efafc3696c6fb44
Никита Троицкий <justtsun@gmail.com>
2024-11-13 23:27:22 +0300

    some features updates

---
+++ Assets/Scripts/GoNextLevel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoNextLevel : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("Last_level", SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
